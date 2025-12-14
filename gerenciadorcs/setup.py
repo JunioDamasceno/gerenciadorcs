475f59dbc21656f6bb67cb0218feb6406fd743cf
Junio da Silva Damasceno <62802088+JunioDamasceno@users.noreply.github.com>
2025-12-13 23:59:52 -0300

    Add files via upload

--- gerenciadorcs/setup.py
+++ gerenciadorcs/setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup
 
-setup( name = 'gerenciadorcs',
-       version = '4.0',
-       author = 'Junio da Silva Damasceno',
-       author_email = 'juniowin@yahoo.com.br',
-       packages = ['gerenciadorcs'],
-       data_files=[('glade',
-                    ['gerenciadorcs/interface-en-US.glade',
-                     'gerenciadorcs/interface-pt-BR.glade',
-                     'gerenciadorcs/interface-ES.glade'])],
-       description = 'um gerenciador de contas, usuários e senhas',
-       url = 'https://github.com/JunioDamasceno/gerenciadorcs',
-       license = 'MIT',
-       keywords = ['gerenciador de senhas', 'Key manager', 'keymanager'],
-       classifiers = [
-           'Development Status :: 5 - Production/Stable',
-           'Intended Audience :: Developers',
-           'Licence :: OSI Aproved :: MIT licence',
-           'Natural Language :: Portuguese (Brazilian)',
+setup( 
+    name = 'gerenciadorcs',
+    version = '4.0',
+    author = 'Junio da Silva Damasceno',
+    author_email = 'juniowin@yahoo.com.br',
+    packages = ['gerenciadorcs'],
+    package_data={
+        'gerenciadorcs': [
+            'interface-en-US.glade',
+            'interface-pt-BR.glade',
+            'interface-ES.glade'
+        ]
+    },
+    include_package_data=True,
+    description = 'um gerenciador de contas, usuários e senhas',
+    url = 'https://github.com/JunioDamasceno/gerenciadorcs',
+    license = 'MIT',
+    keywords = ['gerenciador de senhas', 'Key manager', 'keymanager'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Licence :: OSI Aproved :: MIT Licence',
+        'Natural Language :: Portuguese (Brazilian)',
        ]
 )
 
