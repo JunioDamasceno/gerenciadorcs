92d5207e34fab2dab1ff50a025f5c92c736e09ab
Junio da Silva Damasceno <62802088+JunioDamasceno@users.noreply.github.com>
2026-03-16 07:49:59 -0300

    Add files via upload

--- gerenciadorcs/criar_caminho.py
+++ gerenciadorcs/criar_caminho.py
@@ -3,52 +3,26 @@
 
 import os
 import getpass
+import shutil
 
 def verificar_arquivos():
     usuario = getpass.getuser()
-    caminho = '/home/' + usuario + '/snap/gerenciadorcs/current/bin'
-    arquivo_ac = caminho + '/ac.bin'
-    arquivo_asu = caminho + '/asu.bin'
-    arquivo_nu = caminho + '/nu.bin'
-    arquivo_se = caminho + '/se.bin'
-    arquivo_sx = caminho + '/sx.bin'
-    arquivo_ux = caminho + '/ux.bin'
+    caminho_antigo = '/home/' + usuario + '/snap/gerenciadorcs/current/bin'
+    snap_data = os.environ.get('SNAP_USER_DATA', os.path.expanduser('~'))
 
-    counter_caminho = 0
-    counter_arquivos = 0
+    arquivo_antigo = ['ac.bin', 'asu.bin', 'nu.bin', 'se.bin', 'sx.bin', 'ux.bin']
 
-    if not os.path.exists(caminho):
-        os.makedirs(caminho)
-        counter_caminho = counter_caminho + 1
+    if not os.path.exists(snap_data + '/.gerenciadorcs4-0'):
+        os.makedirs(snap_data + '/.gerenciadorcs4-0')
 
-    if not os.path.exists(arquivo_ac):
-        open(arquivo_ac, 'w')
-        counter_arquivos = counter_arquivos + 1
-
-    if not os.path.exists(arquivo_asu):
-        open(arquivo_asu, 'w')
-        counter_arquivos = counter_arquivos + 1
-
-    if not os.path.exists(arquivo_nu):
-        open(arquivo_nu, 'w')
-        counter_arquivos = counter_arquivos + 1
-
-    if not os.path.exists(arquivo_se):
-        open(arquivo_se, 'w')
-        counter_arquivos = counter_arquivos + 1
-
-    if not os.path.exists(arquivo_sx):
-        open(arquivo_sx, 'w')
-        counter_arquivos = counter_arquivos + 1
-
-    if not os.path.exists(arquivo_ux):
-        open(arquivo_ux, 'w')
-        counter_arquivos = counter_arquivos + 1
-
-    if (counter_caminho == 1 and counter_arquivos == 6):
-        print('os arquivos necessários foram criados')
-
-    else:
-        print('os arquivos necessários não foram criados, pois já existem')
-        
+    for i in arquivo_antigo:
+        if not os.path.exists(caminho_antigo + '/{}'.format(i)):
+            print('não existe')
+            open(snap_data + '/.gerenciadorcs4-0' + '/{}'.format(i), 'w')
+            print('Arquivo {} foi criado.'.format(i))
+        else:
+            print('existe')
+            shutil.move(caminho_antigo + '/{}'.format(i), snap_data + '/.gerenciadorcs4-0' + '/{}'.format(i))
+            print('O arquivo {} não foi criado porque já existe.'.format(i))
+      
 verificar_arquivos()
